5f7d36cde4fcf28fa7ad8575372a58b8e610393b
Jader Dias <jader.dias@booking.com>
2015-05-26 19:35:04 +0200

    minor fix in the test for movingMedian

--- expr_test.go
+++ expr_test.go
@@ -265,7 +265,7 @@ func TestEvalExpression(t *testing.T) {
 			map[metricRequest][]*metricData{
 				metricRequest{"metric1", 0, 0}: []*metricData{makeResponse("metric1", []float64{1, 1, 1, 1, 2, 2, 2, 4, 6, 4, 6, 8}, 1, now32)},
 			},
-			[]float64{math.NaN(), math.NaN(), math.NaN(), math.NaN(), 1, 1, 1.5, 2, 2, 3, 4, 5, 6},
+			[]float64{math.NaN(), math.NaN(), math.NaN(), math.NaN(), 1, 1, 1.5, 2, 2, 3, 4, 5, 5, 6},
 			"movingMedian(metric1,4)",
 		},
 		{
