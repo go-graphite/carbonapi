8f435f857c4718fee6c9232771b1679ccb62add5
Stefan Boronea <stefan.boronea@gmail.com>
2015-08-11 17:15:51 +0200

    Add the isNonNull function.
    http://graphite.readthedocs.org/en/latest/functions.html#graphite.render.functions.isNonNull

--- expr.go
+++ expr.go
@@ -935,6 +935,29 @@ func evalExpr(e *expr, from, until int32, values map[metricRequest][]*metricData
 
 		return results
 
+	case "isNonNull", "isNotNull": // isNonNull(seriesList), isNotNull(seriesList)
+
+		_, err := getSeriesArg(e.args[0], from, until, values)
+
+		e.target = "isNonNull"
+
+		if err != nil {
+			return nil
+		}
+
+		return forEachSeriesDo(e, from, until, values, func(a *metricData, r *metricData) *metricData {
+			for i, _ := range a.Values {
+				r.IsAbsent[i] = false
+				if a.IsAbsent[i] {
+					r.Values[i] = 0
+				} else {
+					r.Values[i] = 1
+				}
+
+			}
+			return r
+		})
+
 	case "lowestAverage", "lowestCurrent": // lowestAverage(seriesList, n) , lowestCurrent(seriesList, n)
 
 		arg, err := getSeriesArg(e.args[0], from, until, values)
