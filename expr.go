dfc600b852f899c92f88ee9054580d4732895d6f
Zack Hsi <zhsi@lyft.com>
2015-10-21 00:38:29 -0700

    Fail a point if its a member of <windows> consecutive failed points

--- expr.go
+++ expr.go
@@ -801,10 +801,10 @@ func evalExpr(e *expr, from, until int32, values map[metricRequest][]*metricData
 			r.drawAsInfinite = true
 			r.secondYAxis = true
 
+			single_failures := make([]int, len(r.Values))
 			for i, v := range a.Values {
 				if a.IsAbsent[i] {
-					r.Values[i] = 0
-					r.IsAbsent[i] = true
+					single_failures[i] = 0
 					continue
 				}
 
@@ -813,6 +813,49 @@ func evalExpr(e *expr, from, until int32, values map[metricRequest][]*metricData
 				stdevsAway := math.Abs((v - average) / stdev)
 
 				if stdevsAway < acceptableStdevs {
+					single_failures[i] = 0
+				} else {
+					single_failures[i] = 1
+				}
+			}
+
+			left_failures := make([]int, len(r.Values))
+			failures := 0
+			for i, v := range single_failures {
+				left_failures[i] = failures
+				if v == 1 {
+					failures++
+				} else {
+					failures = 0
+				}
+			}
+
+			right_failures := make([]int, len(r.Values))
+			failures = 0
+			for i := range single_failures {
+				reverseI := len(single_failures) - i - 1
+				right_failures[reverseI] = failures
+				if single_failures[reverseI] == 1 {
+					failures++
+				} else {
+					failures = 0
+				}
+			}
+
+			for i, v := range single_failures {
+				if v == 0 {
+					continue
+				}
+
+				failures := 1
+				if i-1 >= 0 {
+					failures += left_failures[i-1]
+				}
+				if i+1 < len(single_failures) {
+					failures += right_failures[i+1]
+				}
+
+				if failures < windows {
 					r.Values[i] = 0
 				} else {
 					r.Values[i] = 1
