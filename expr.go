5173d74588564f1525ad242e812a1658351948ce
Slawomir Skowron <szibis@gmail.com>
2015-10-25 17:34:41 +0100

    Add offset function support

--- expr.go
+++ expr.go
@@ -1739,6 +1739,35 @@ func evalExpr(e *expr, from, until int32, values map[metricRequest][]*metricData
 
 		return results
 
+	case "offset": // offset(seriesList,factor)
+		arg, err := getSeriesArg(e.args[0], from, until, values)
+		if err != nil {
+			return nil
+		}
+		factor, err := getFloatArg(e, 1)
+		if err != nil {
+			return nil
+		}
+		var results []*metricData
+
+		for _, a := range arg {
+			r := *a
+			r.Name = proto.String(fmt.Sprintf("offset(%s,%g)", a.GetName(), factor))
+			r.Values = make([]float64, len(a.Values))
+			r.IsAbsent = make([]bool, len(a.Values))
+
+			for i, v := range a.Values {
+				if a.IsAbsent[i] {
+					r.Values[i] = 0
+					r.IsAbsent[i] = true
+					continue
+				}
+				r.Values[i] = v + factor
+			}
+			results = append(results, &r)
+		}
+		return results
+
 	case "offsetToZero": // offsetToZero(seriesList)
 		return forEachSeriesDo(e, from, until, values, func(a *metricData, r *metricData) *metricData {
 			minimum := math.Inf(1)
@@ -1757,6 +1786,7 @@ func evalExpr(e *expr, from, until int32, values map[metricRequest][]*metricData
 			}
 			return r
 		})
+
 	case "scale": // scale(seriesList, factor)
 		arg, err := getSeriesArg(e.args[0], from, until, values)
 		if err != nil {
