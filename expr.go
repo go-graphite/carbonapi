d09caf7c5c852c2aa715258040e2c9db1e8db8e9
Slawomir Skowron <szibis@gmail.com>
2015-10-26 11:52:01 +0100

    Add squrareRoot function

--- expr.go
+++ expr.go
@@ -2365,6 +2365,31 @@ func evalExpr(e *expr, from, until int32, values map[metricRequest][]*metricData
 			results = append(results, &r)
 		}
 		return results
+
+	case "squareRoot": // squareRoot(seriesList)
+		arg, err := getSeriesArg(e.args[0], from, until, values)
+		if err != nil {
+			return nil
+		}
+		var results []*metricData
+
+		for _, a := range arg {
+			r := *a
+			r.Name = proto.String(fmt.Sprintf("squareRoot(%s)", a.GetName()))
+			r.Values = make([]float64, len(a.Values))
+			r.IsAbsent = make([]bool, len(a.Values))
+
+			for i, v := range a.Values {
+				if a.IsAbsent[i] {
+					r.Values[i] = 0
+					r.IsAbsent[i] = true
+					continue
+				}
+				r.Values[i] = math.Sqrt(v)
+			}
+			results = append(results, &r)
+		}
+		return results
 	}
 
 	logger.Logf("unknown function in evalExpr: %q\n", e.target)
