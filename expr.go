4a76c711f6535d63e4e43fe09b459eaef884f72d
Slawomir Skowron <szibis@gmail.com>
2015-10-31 21:12:56 +0100

    Add changed function

--- expr.go
+++ expr.go
@@ -1220,6 +1220,35 @@ func evalExpr(e *expr, from, until int32, values map[metricRequest][]*metricData
 		}
 		return results
 
+	case "changed": // changed(SeriesList)
+		args, err := getSeriesArg(e.args[0], from, until, values)
+		if err != nil {
+			return nil
+		}
+
+		var result []*metricData
+		for _, a := range args {
+			r := *a
+			r.Name = proto.String(fmt.Sprintf("%s(%s)", e.target, a.GetName()))
+			r.Values = make([]float64, len(a.Values))
+			r.IsAbsent = make([]bool, len(a.Values))
+
+			prev := math.NaN()
+			for i, v := range a.Values {
+				if math.IsNaN(prev) {
+					prev = v
+					r.Values[i] = 0
+				} else if !math.IsNaN(v) && prev != v {
+					r.Values[i] = 1
+					prev = v
+				} else {
+					r.Values[i] = 0
+				}
+			}
+			result = append(result, &r)
+		}
+		return result
+
 	case "kolmogorovSmirnovTest2", "ksTest2": // ksTest2(series, series, points|"interval")
 		arg1, err := getSeriesArg(e.args[0], from, until, values)
 		if err != nil {
