9c7148cefe8cc999ed54dfce7789c8448e82d79a
Mathias Gug <mathias@lyft.com>
2015-11-07 10:18:02 -0800

    Include zero when calculating an integral.

--- expr.go
+++ expr.go
@@ -1215,7 +1215,7 @@ func evalExpr(e *expr, from, until int32, values map[metricRequest][]*metricData
 		return forEachSeriesDo(e, from, until, values, func(a *metricData, r *metricData) *metricData {
 			current := 0.0
 			for i, v := range a.Values {
-				if a.IsAbsent[i] || v == 0 {
+				if a.IsAbsent[i] {
 					r.Values[i] = 0
 					r.IsAbsent[i] = true
 					continue
