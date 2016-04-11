go-graphite/carbonapi

commit 85e0c21f6af53f8afb742dcd6e13caf8e44e5cc5

propagate eval errors to users
before the patch:

$ http_proxy= curl -v 'http://127.0.0.1:8082/render?format=json&target=foobar(foo)'
* About to connect() to 127.0.0.1 port 8082 (#0)
*   Trying 127.0.0.1...
* Connected to 127.0.0.1 (127.0.0.1) port 8082 (#0)
> GET /render?format=json&target=foobar(foo) HTTP/1.1
> User-Agent: curl/7.29.0
> Host: 127.0.0.1:8082
> Accept: */*
>
< HTTP/1.1 500 Internal Server Error
< Access-Control-Allow-Methods: POST, GET, OPTIONS
< Access-Control-Allow-Origin: *
< Content-Type: text/plain; charset=utf-8
< X-Content-Type-Options: nosniff
< Date: Wed, 02 Mar 2016 16:17:40 GMT
< Content-Length: 35
<
Internal Server Error: eval failed
* Connection #0 to host 127.0.0.1 left intact

after the patch:

$ http_proxy= curl -v 'http://127.0.0.1:8082/render?format=json&target=foo(bar)&target=tukeyAbove(1,2,3)&target=pearsonClosest(foo,bar,baz,quux)'
* About to connect() to 127.0.0.1 port 8082 (#0)
*   Trying 127.0.0.1...
* Connected to 127.0.0.1 (127.0.0.1) port 8082 (#0)
> GET /render?format=json&target=foo(bar)&target=tukeyAbove(1,2,3)&target=pearsonClosest(foo,bar,baz,quux) HTTP/1.1
> User-Agent: curl/7.29.0
> Host: 127.0.0.1:8082
> Accept: */*
>
< HTTP/1.1 400 Bad Request
< Access-Control-Allow-Methods: POST, GET, OPTIONS
< Access-Control-Allow-Origin: *
< Content-Type: text/plain; charset=utf-8
< X-Content-Type-Options: nosniff
< Date: Thu, 24 Mar 2016 14:43:58 GMT
< Content-Length: 172
<
Encountered the following errors:
foo(bar): unknown function in evalExpr: "foo"
tukeyAbove(1,2,3): missing time series
pearsonClosest(foo,bar,baz,quux): too many arguments
* Connection #0 to host 127.0.0.1 left intact

fixes #75

main.go
@@ -197,6 +197,7 @@ func renderHandler(w http.ResponseWriter, r *http.Request, stats *renderStats) {
 	}
 
 	var results []*metricData
+	var errors []string
 	metricMap := make(map[metricRequest][]*metricData)
 
 	for _, target := range targets {
@@ -285,9 +286,19 @@ func renderHandler(w http.ResponseWriter, r *http.Request, stats *renderStats) {
 					logger.Logf("panic during eval: %s: %s\n%s\n", cacheKey, r, string(buf[:]))
 				}
 			}()
-			exprs := evalExpr(exp, from32, until32, metricMap)
+			exprs, err := evalExpr(exp, from32, until32, metricMap)
+			if err != nil {
+				errors = append(errors, target+": "+err.Error())
+				return
+			}
 			results = append(results, exprs...)
 		}()
+	}
+
+	if len(errors) > 0 {
+		errors = append([]string{"Encountered the following errors:"}, errors...)
+		http.Error(w, strings.Join(errors, "\n"), http.StatusBadRequest)
+		return
 	}
 
 	var body []byte

expr_test.go
@@ -1973,9 +1973,9 @@ func TestEvalExpression(t *testing.T) {
 			oldValues[key] = entry
 		}
 
-		g := evalExpr(tt.e, 0, 1, tt.m)
-		if g == nil {
-			t.Errorf("failed to eval %s", tt.e.target)
+		g, err := evalExpr(tt.e, 0, 1, tt.m)
+		if err != nil {
+			t.Errorf("failed to eval %s: %s", tt.e.target, err)
 			continue
 		}
 		if len(g) != len(tt.want) {
@@ -2431,9 +2431,9 @@ func TestEvalSummarize(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		g := evalExpr(tt.e, 0, 1, tt.m)
-		if g == nil {
-			t.Errorf("failed to eval %v", tt.name)
+		g, err := evalExpr(tt.e, 0, 1, tt.m)
+		if err != nil {
+			t.Errorf("failed to eval %v: %s", tt.name, err)
 			continue
 		}
 		if g[0].GetStepTime() != tt.step {
@@ -2876,9 +2876,9 @@ func TestEvalMultipleReturns(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		g := evalExpr(tt.e, 0, 1, tt.m)
-		if g == nil {
-			t.Errorf("failed to eval %v", tt.name)
+		g, err := evalExpr(tt.e, 0, 1, tt.m)
+		if err != nil {
+			t.Errorf("failed to eval %v: %s", tt.name, err)
 			continue
 		}
 		if g[0] == nil {
@@ -3008,9 +3008,9 @@ func TestEvalCustomFromUntil(t *testing.T) {
 			oldValues[key] = entry
 		}
 
-		g := evalExpr(tt.e, tt.from, tt.until, tt.m)
-		if g == nil {
-			t.Errorf("failed to eval %v", tt.name)
+		g, err := evalExpr(tt.e, tt.from, tt.until, tt.m)
+		if err != nil {
+			t.Errorf("failed to eval %v: %s", tt.name, err)
 			continue
 		}
 		if g[0] == nil {

commit 95e7ee868cd27ecf50038276f2afcc7cdd5bf590

fix a regression caused by 85e0c21
in 85e0c21 I introduced a check that resulted in 400 Bad Request responses for
some queries that included non-existent metrics

the root cause of the regression is that I didn't distinguish between not
specifying a timeseries argument and specifying one that yields no datapoints

the correct behaviour is to exclude targets with no data from the final result
without producing an error (at least this is what vanilla graphite does)

broken example:

$ http_proxy= curl -v 'http://127.0.0.1:8082/render?format=json&target=transformNull(does.not.exist,1)'
* About to connect() to 127.0.0.1 port 8082 (#0)
*   Trying 127.0.0.1...
* Connected to 127.0.0.1 (127.0.0.1) port 8082 (#0)
> GET /render?format=json&target=transformNull(does.not.exist,1) HTTP/1.1
> User-Agent: curl/7.29.0
> Host: 127.0.0.1:8082
> Accept: */*
>
< HTTP/1.1 400 Bad Request
< Access-Control-Allow-Methods: POST, GET, OPTIONS
< Access-Control-Allow-Origin: *
< Content-Type: text/plain; charset=utf-8
< X-Content-Type-Options: nosniff
< Date: Mon, 11 Apr 2016 17:48:12 GMT
< Content-Length: 87
<
Encountered the following errors:
transformNull(does.not.exist,1): missing time series
* Connection #0 to host 127.0.0.1 left intact

correct example:

$ http_proxy= curl -v 'http://127.0.0.1:8082/render?format=json&target=transformNull(does.not.exist,1)'
* About to connect() to 127.0.0.1 port 8082 (#0)
*   Trying 127.0.0.1...
* Connected to 127.0.0.1 (127.0.0.1) port 8082 (#0)
> GET /render?format=json&target=transformNull(does.not.exist,1) HTTP/1.1
> User-Agent: curl/7.29.0
> Host: 127.0.0.1:8082
> Accept: */*
>
< HTTP/1.1 200 OK
< Access-Control-Allow-Methods: POST, GET, OPTIONS
< Access-Control-Allow-Origin: *
< Content-Type: application/json
< Date: Mon, 11 Apr 2016 17:51:33 GMT
< Content-Length: 2
<
* Connection #0 to host 127.0.0.1 left intact
[]

refers #75

expr.go
@@ -315,9 +315,10 @@ func parseString(s string) (string, string, error) {
 }
 
 var (
-	ErrBadType           = errors.New("bad type")
-	ErrMissingArgument   = errors.New("missing argument")
-	ErrMissingTimeseries = errors.New("missing time series")
+	ErrBadType            = errors.New("bad type")
+	ErrMissingArgument    = errors.New("missing argument")
+	ErrMissingTimeseries  = errors.New("missing time series argument")
+	ErrSeriesDoesNotExist = errors.New("no timeseries with that name")
 )
 
 func getStringArg(e *expr, n int) (string, error) {
@@ -492,7 +493,7 @@ func getSeriesArg(arg *expr, from, until int32, values map[metricRequest][]*metr
 	a, _ := evalExpr(arg, from, until, values)
 
 	if len(a) == 0 {
-		return nil, ErrMissingTimeseries
+		return nil, ErrSeriesDoesNotExist
 	}
 
 	return a, nil
@@ -511,7 +512,7 @@ func getSeriesArgs(e []*expr, from, until int32, values map[metricRequest][]*met
 	}
 
 	if len(args) == 0 {
-		return nil, ErrMissingTimeseries
+		return nil, ErrSeriesDoesNotExist
 	}
 
 	return args, nil

main.go
@@ -287,7 +287,7 @@ func renderHandler(w http.ResponseWriter, r *http.Request, stats *renderStats) {
 				}
 			}()
 			exprs, err := evalExpr(exp, from32, until32, metricMap)
-			if err != nil {
+			if err != nil && err != ErrSeriesDoesNotExist {
 				errors = append(errors, target+": "+err.Error())
 				return
 			}