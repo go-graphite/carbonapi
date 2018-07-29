65e47d4dcd54e04423066e2677f0936dbd79e4dc
Gyanendra Singh <gyanendra1810@gmail.com>
2018-07-29 14:32:49 +0200

    Remove extraneous config limiter removal.

--- cmd/carbonapi/http_handlers.go
+++ cmd/carbonapi/http_handlers.go
@@ -300,7 +300,6 @@ func renderHandler(w http.ResponseWriter, r *http.Request) {
 			r, err := config.zipper.Render(ctx, req)
 			if err != nil {
 				errors[target] = err.Error()
-				config.limiter.leave()
 			}
 
 			config.limiter.leave()
