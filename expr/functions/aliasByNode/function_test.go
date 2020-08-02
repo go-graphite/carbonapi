8c113d04b7d11ff2df6ba2f77c4a15763c49de00
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2020-08-02 23:32:01 +0200

    Make aliasByNode and aliasByTags the same function

--- expr/functions/aliasByNode/function_test.go
+++ expr/functions/aliasByNode/function_test.go
@@ -1,6 +1,7 @@
 package aliasByNode
 
 import (
+	"math"
 	"testing"
 	"time"
 
@@ -9,16 +10,23 @@ import (
 	"github.com/go-graphite/carbonapi/expr/types"
 	"github.com/go-graphite/carbonapi/pkg/parser"
 	th "github.com/go-graphite/carbonapi/tests"
+
+	"github.com/go-graphite/carbonapi/expr/functions/perSecond"
 )
 
 func init() {
 	md := New("")
-	evaluator := th.EvaluatorFromFunc(md[0].F)
-	metadata.SetEvaluator(evaluator)
-	helper.SetEvaluator(evaluator)
 	for _, m := range md {
 		metadata.RegisterFunction(m.Name, m.F)
 	}
+	psFunc := perSecond.New("")
+	for _, m := range psFunc {
+		metadata.RegisterFunction(m.Name, m.F)
+	}
+
+	evaluator := th.EvaluatorFromFuncWithMetadata(metadata.FunctionMD.Functions)
+	metadata.SetEvaluator(evaluator)
+	helper.SetEvaluator(evaluator)
 }
 
 func TestAliasByNode(t *testing.T) {
@@ -47,6 +55,41 @@ func TestAliasByNode(t *testing.T) {
 			},
 			[]*types.MetricData{types.MakeMetricData("foo.bar",
 				[]float64{1, 2, 3, 4, 5}, 1, now32)},
+		},
+		{
+			`aliasByTags(*, "foo")`,
+			map[parser.MetricRequest][]*types.MetricData{
+				{"*", 0, 1}: {types.MakeMetricData("metric1.foo.bar.baz;foo=bar;baz=bam", []float64{1, 2, 3, 4, 5}, 1, now32)},
+			},
+			[]*types.MetricData{types.MakeMetricData("bar", []float64{1, 2, 3, 4, 5}, 1, now32)},
+		},
+		{
+			`aliasByTags(*, "foo", "name")`,
+			map[parser.MetricRequest][]*types.MetricData{
+				{"*", 0, 1}: {types.MakeMetricData("metric1;foo=bar", []float64{1, 2, 3, 4, 5}, 1, now32)},
+			},
+			[]*types.MetricData{types.MakeMetricData("bar.metric1", []float64{1, 2, 3, 4, 5}, 1, now32)},
+		},
+		{
+			`aliasByTags(*, 2, "blah", "foo", 1)`,
+			map[parser.MetricRequest][]*types.MetricData{
+				{"*", 0, 1}: {types.MakeMetricData("base.metric1;foo=bar;baz=bam", []float64{1, 2, 3, 4, 5}, 1, now32)},
+			},
+			[]*types.MetricData{types.MakeMetricData(".bar.metric1", []float64{1, 2, 3, 4, 5}, 1, now32)},
+		},
+		{
+			`aliasByTags(*, 2, "baz", "foo", 1)`,
+			map[parser.MetricRequest][]*types.MetricData{
+				{"*", 0, 1}: {types.MakeMetricData("base.metric1;foo=bar;baz=bam", []float64{1, 2, 3, 4, 5}, 1, now32)},
+			},
+			[]*types.MetricData{types.MakeMetricData("bam.bar.metric1", []float64{1, 2, 3, 4, 5}, 1, now32)},
+		},
+		{
+			`aliasByTags(perSecond(*), 'name')`,
+			map[parser.MetricRequest][]*types.MetricData{
+				{"*", 0, 1}: {types.MakeMetricData("base.metric1;foo=bar;baz=bam", []float64{1, 2, 3, 4, 5}, 1, now32)},
+			},
+			[]*types.MetricData{types.MakeMetricData("base.metric1", []float64{math.NaN(), 1, 1, 1, 1}, 1, now32)},
 		},
 	}
 
