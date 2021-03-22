06542c746b3d2bfd133ca0cd5644a8740585e05e
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2021-03-23 00:28:59 +0100

    Add xFileFactor support to SummarizeValues

--- expr/consolidations/consolidations_test.go
+++ expr/consolidations/consolidations_test.go
@@ -8,100 +8,129 @@ import (
 func TestSummarizeValues(t *testing.T) {
 	epsilon := math.Nextafter(1, 2) - 1
 	tests := []struct {
-		name     string
-		function string
-		values   []float64
-		expected float64
+		name         string
+		function     string
+		values       []float64
+		xFilesFactor float32
+		expected     float64
 	}{
 		{
-			name:     "no values",
-			function: "sum",
-			values:   []float64{},
-			expected: math.NaN(),
+			name:         "no values",
+			function:     "sum",
+			values:       []float64{},
+			xFilesFactor: 0,
+			expected:     math.NaN(),
 		},
 		{
-			name:     "sum",
-			function: "sum",
-			values:   []float64{1, 2, 3},
-			expected: 6,
+			name:         "sum",
+			function:     "sum",
+			values:       []float64{1, 2, 3},
+			xFilesFactor: 0,
+			expected:     6,
 		},
 		{
-			name:     "sum alias",
-			function: "total",
-			values:   []float64{1, 2, 3},
-			expected: 6,
+			name:         "sum alias",
+			function:     "total",
+			values:       []float64{1, 2, 3},
+			xFilesFactor: 0,
+			expected:     6,
 		},
 		{
-			name:     "avg",
-			function: "avg",
-			values:   []float64{1, 2, 3, 4},
-			expected: 2.5,
+			name:         "avg",
+			function:     "avg",
+			values:       []float64{1, 2, 3, 4},
+			xFilesFactor: 0,
+			expected:     2.5,
 		},
 		{
-			name:     "max",
-			function: "max",
-			values:   []float64{1, 2, 3, 4},
-			expected: 4,
+			name:         "avg with nones",
+			function:     "avg",
+			values:       []float64{1, 2, 3, 4, math.NaN()},
+			xFilesFactor: 0,
+			expected:     2.5,
 		},
 		{
-			name:     "min",
-			function: "min",
-			values:   []float64{1, 2, 3, 4},
-			expected: 1,
+			name:         "avg xFilesFactor",
+			function:     "avg",
+			values:       []float64{1, 2, 3, 4, math.NaN()},
+			xFilesFactor: 0.9,
+			expected:     math.NaN(),
 		},
 		{
-			name:     "last",
-			function: "last",
-			values:   []float64{1, 2, 3, 4},
-			expected: 4,
+			name:         "max",
+			function:     "max",
+			values:       []float64{1, 2, 3, 4},
+			xFilesFactor: 0,
+			expected:     4,
 		},
 		{
-			name:     "range",
-			function: "range",
-			values:   []float64{1, 2, 3, 4},
-			expected: 3,
+			name:         "min",
+			function:     "min",
+			values:       []float64{1, 2, 3, 4},
+			xFilesFactor: 0,
+			expected:     1,
 		},
 		{
-			name:     "median",
-			function: "median",
-			values:   []float64{1, 2, 3, 10, 11},
-			expected: 3,
+			name:         "last",
+			function:     "last",
+			values:       []float64{1, 2, 3, 4},
+			xFilesFactor: 0,
+			expected:     4,
 		},
 		{
-			name:     "multiply",
-			function: "multiply",
-			values:   []float64{1, 2, 3, 4},
-			expected: 24,
+			name:         "range",
+			function:     "range",
+			values:       []float64{1, 2, 3, 4},
+			xFilesFactor: 0,
+			expected:     3,
 		},
 		{
-			name:     "diff",
-			function: "diff",
-			values:   []float64{1, 2, 3, 4},
-			expected: -8,
+			name:         "median",
+			function:     "median",
+			values:       []float64{1, 2, 3, 10, 11},
+			xFilesFactor: 0,
+			expected:     3,
 		},
 		{
-			name:     "count",
-			function: "count",
-			values:   []float64{1, 2, 3, 4},
-			expected: 4,
+			name:         "multiply",
+			function:     "multiply",
+			values:       []float64{1, 2, 3, 4},
+			xFilesFactor: 0,
+			expected:     24,
 		},
 		{
-			name:     "stddev",
-			function: "stddev",
-			values:   []float64{1, 2, 3, 4},
-			expected: 1.118033988749895,
+			name:         "diff",
+			function:     "diff",
+			values:       []float64{1, 2, 3, 4},
+			xFilesFactor: 0,
+			expected:     -8,
 		},
 		{
-			name:     "p50 (fallback)",
-			function: "p50",
-			values:   []float64{1, 2, 3, 10, 11},
-			expected: 3,
+			name:         "count",
+			function:     "count",
+			values:       []float64{1, 2, 3, 4},
+			xFilesFactor: 0,
+			expected:     4,
+		},
+		{
+			name:         "stddev",
+			function:     "stddev",
+			values:       []float64{1, 2, 3, 4},
+			xFilesFactor: 0,
+			expected:     1.118033988749895,
+		},
+		{
+			name:         "p50 (fallback)",
+			function:     "p50",
+			values:       []float64{1, 2, 3, 10, 11},
+			xFilesFactor: 0,
+			expected:     3,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			actual := SummarizeValues(tt.function, tt.values)
+			actual := SummarizeValues(tt.function, tt.values, tt.xFilesFactor)
 			if math.Abs(actual-tt.expected) > epsilon {
 				t.Errorf("actual %v, expected %v", actual, tt.expected)
 			}
