bb33f26c8466deb9c9134c1085d6fcf77cc02205
msaf1980 <msaf1980@gmail.com>
2021-04-13 12:05:35 +0500

    simplify zipper error handling

--- zipper/errors/errors.go
+++
@@ -1,9 +0,0 @@
-package errors
-
-import (
-	"github.com/ansel1/merry"
-)
-
-var ErrBackendError = merry.New("error fetching data from backend")
-var ErrResponseTypeMismatch = merry.New("type for the response doesn't match what's expected")
-var ErrTimeout = merry.New("timeout while fetching data")
