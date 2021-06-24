89701352bff2adc8a422408cabbb8d3e9115b6d6
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2021-06-24 18:06:42 +0200

    Fix deepsource issues

--- expr/functions/groupByNode/function_test.go
+++ expr/functions/groupByNode/function_test.go
@@ -30,83 +30,85 @@ func init() {
 func TestGroupByNode(t *testing.T) {
 	now32 := int64(time.Now().Unix())
 
+	mr := parser.MetricRequest{Metric: "metric1.foo.*.*", From: 0, Until: 1}
+
 	tests := []th.MultiReturnEvalTestItem{
 		{
-			"groupByNode(metric1.foo.*.*,3,\"sum\")",
-			map[parser.MetricRequest][]*types.MetricData{
-				{"metric1.foo.*.*", 0, 1}: {
+			Target: "groupByNode(metric1.foo.*.*,3,\"sum\")",
+			M: map[parser.MetricRequest][]*types.MetricData{
+				mr: {
 					types.MakeMetricData("metric1.foo.bar1.baz", []float64{1, 2, 3, 4, 5}, 1, now32),
 					types.MakeMetricData("metric1.foo.bar1.qux", []float64{6, 7, 8, 9, 10}, 1, now32),
 					types.MakeMetricData("metric1.foo.bar2.baz", []float64{11, 12, 13, 14, 15}, 1, now32),
 					types.MakeMetricData("metric1.foo.bar2.qux", []float64{7, 8, 9, 10, 11}, 1, now32),
 				},
 			},
-			"groupByNode",
-			map[string][]*types.MetricData{
+			Name: "groupByNode",
+			Results: map[string][]*types.MetricData{
 				"baz": {types.MakeMetricData("baz", []float64{12, 14, 16, 18, 20}, 1, now32)},
 				"qux": {types.MakeMetricData("qux", []float64{13, 15, 17, 19, 21}, 1, now32)},
 			},
 		},
 		{
-			"groupByNode(metric1.foo.*.*,3,\"sum\")",
-			map[parser.MetricRequest][]*types.MetricData{
-				{"metric1.foo.*.*", 0, 1}: {
+			Target: "groupByNode(metric1.foo.*.*,3,\"sum\")",
+			M: map[parser.MetricRequest][]*types.MetricData{
+				mr: {
 					types.MakeMetricData("metric1.foo.bar1.01", []float64{1, 2, 3, 4, 5}, 1, now32),
 					types.MakeMetricData("metric1.foo.bar1.10", []float64{6, 7, 8, 9, 10}, 1, now32),
 					types.MakeMetricData("metric1.foo.bar2.01", []float64{11, 12, 13, 14, 15}, 1, now32),
 					types.MakeMetricData("metric1.foo.bar2.10", []float64{7, 8, 9, 10, 11}, 1, now32),
 				},
 			},
-			"groupByNode_names_with_int",
-			map[string][]*types.MetricData{
+			Name: "groupByNode_names_with_int",
+			Results: map[string][]*types.MetricData{
 				"01": {types.MakeMetricData("01", []float64{12, 14, 16, 18, 20}, 1, now32)},
 				"10": {types.MakeMetricData("10", []float64{13, 15, 17, 19, 21}, 1, now32)},
 			},
 		},
 		{
-			"groupByNode(metric1.foo.*.*,3,\"sum\")",
-			map[parser.MetricRequest][]*types.MetricData{
-				{"metric1.foo.*.*", 0, 1}: {
+			Target: "groupByNode(metric1.foo.*.*,3,\"sum\")",
+			M: map[parser.MetricRequest][]*types.MetricData{
+				mr: {
 					types.MakeMetricData("metric1.foo.bar1.127_0_0_1:2003", []float64{1, 2, 3, 4, 5}, 1, now32),
 					types.MakeMetricData("metric1.foo.bar1.127_0_0_1:2004", []float64{6, 7, 8, 9, 10}, 1, now32),
 					types.MakeMetricData("metric1.foo.bar2.127_0_0_1:2003", []float64{11, 12, 13, 14, 15}, 1, now32),
 					types.MakeMetricData("metric1.foo.bar2.127_0_0_1:2004", []float64{7, 8, 9, 10, 11}, 1, now32),
 				},
 			},
-			"groupByNode_names_with_colons",
-			map[string][]*types.MetricData{
+			Name: "groupByNode_names_with_colons",
+			Results: map[string][]*types.MetricData{
 				"127_0_0_1:2003": {types.MakeMetricData("127_0_0_1:2003", []float64{12, 14, 16, 18, 20}, 1, now32)},
 				"127_0_0_1:2004": {types.MakeMetricData("127_0_0_1:2004", []float64{13, 15, 17, 19, 21}, 1, now32)},
 			},
 		},
 		{
-			"groupByNode(metric1.foo.*.*,-2,\"sum\")",
-			map[parser.MetricRequest][]*types.MetricData{
-				{"metric1.foo.*.*", 0, 1}: {
+			Target: "groupByNode(metric1.foo.*.*,-2,\"sum\")",
+			M: map[parser.MetricRequest][]*types.MetricData{
+				mr: {
 					types.MakeMetricData("metric1.foo.bar1.baz", []float64{1, 2, 3, 4, 5}, 1, now32),
 					types.MakeMetricData("metric1.foo.bar1.qux", []float64{6, 7, 8, 9, 10}, 1, now32),
 					types.MakeMetricData("metric1.foo.bar2.baz", []float64{11, 12, 13, 14, 15}, 1, now32),
 					types.MakeMetricData("metric1.foo.bar2.qux", []float64{7, 8, 9, 10, 11}, 1, now32),
 				},
 			},
-			"groupByNode_with_negative_index",
-			map[string][]*types.MetricData{
+			Name: "groupByNode_with_negative_index",
+			Results: map[string][]*types.MetricData{
 				"bar1": {types.MakeMetricData("bar1", []float64{7, 9, 11, 13, 15}, 1, now32)},
 				"bar2": {types.MakeMetricData("bar2", []float64{18, 20, 22, 24, 26}, 1, now32)},
 			},
 		},
 		{
-			"groupByNodes(metric1.foo.*.*,\"sum\",0,1,3)",
-			map[parser.MetricRequest][]*types.MetricData{
-				{"metric1.foo.*.*", 0, 1}: {
+			Target: "groupByNodes(metric1.foo.*.*,\"sum\",0,1,3)",
+			M: map[parser.MetricRequest][]*types.MetricData{
+				mr: {
 					types.MakeMetricData("metric1.foo.bar1.baz", []float64{1, 2, 3, 4, 5}, 1, now32),
 					types.MakeMetricData("metric1.foo.bar1.qux", []float64{6, 7, 8, 9, 10}, 1, now32),
 					types.MakeMetricData("metric1.foo.bar2.baz", []float64{11, 12, 13, 14, 15}, 1, now32),
 					types.MakeMetricData("metric1.foo.bar2.qux", []float64{7, 8, 9, 10, 11}, 1, now32),
 				},
 			},
-			"groupByNodes",
-			map[string][]*types.MetricData{
+			Name: "groupByNodes",
+			Results: map[string][]*types.MetricData{
 				"metric1.foo.baz": {types.MakeMetricData("metric1.foo.baz", []float64{12, 14, 16, 18, 20}, 1, now32)},
 				"metric1.foo.qux": {types.MakeMetricData("metric1.foo.qux", []float64{13, 15, 17, 19, 21}, 1, now32)},
 			},
