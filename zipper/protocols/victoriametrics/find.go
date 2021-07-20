9c513dd5db887b0411cc4da948c3841ff56a1d98
Igor Khasilev <igor.khasilev@lifestreet.com>
2021-07-20 23:39:34 +0300

    switch to metrics/find API for VictoriaMetrics v>1.42.0

--- zipper/protocols/victoriametrics/find.go
+++ zipper/protocols/victoriametrics/find.go
@@ -1,7 +1,6 @@
 package victoriametrics
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"net/url"
@@ -31,9 +30,9 @@ func (c *VictoriaMetricsGroup) Find(ctx context.Context, request *protov3.MultiG
 	stats := &types.Stats{}
 	var serverUrl string
 	if len(c.vmClusterTenantID) > 0 {
-		serverUrl = fmt.Sprintf("http://127.0.0.1/select/%s/prometheus/api/v1/query_range", c.vmClusterTenantID)
+		serverUrl = fmt.Sprintf("http://127.0.0.1/select/%s/graphite/metrics/find", c.vmClusterTenantID)
 	} else {
-		serverUrl = "http://127.0.0.1/api/v1/query_range"
+		serverUrl = "http://127.0.0.1/metrics/find"
 	}
 	rewrite, _ := url.Parse(serverUrl)
 
@@ -43,8 +42,7 @@ func (c *VictoriaMetricsGroup) Find(ctx context.Context, request *protov3.MultiG
 
 	for _, query := range request.Metrics {
 		v := url.Values{
-			"query":  []string{query},
-			"format": []string{"json"},
+			"query": []string{query},
 		}
 
 		rewrite.RawQuery = v.Encode()
@@ -63,7 +61,6 @@ func (c *VictoriaMetricsGroup) Find(ctx context.Context, request *protov3.MultiG
 			}
 			continue
 		}
-
 		parsedJSON, err := parser.ParseBytes(res.Response)
 		if err != nil {
 			if e == nil {
@@ -86,15 +83,12 @@ func (c *VictoriaMetricsGroup) Find(ctx context.Context, request *protov3.MultiG
 
 		stats.Servers = append(stats.Servers, res.Server)
 		matches := make([]protov3.GlobMatch, 0, len(globs))
-		var path string
 		for _, m := range globs {
-			b, _ := m.StringBytes()
-			isLeaf := true
-			if bytes.HasSuffix(b, []byte{'.'}) {
-				isLeaf = false
-				path = string(b[:len(b)-1])
-			} else {
-				path = string(b)
+			p := m.GetObject()
+			isLeaf := p.Get("leaf").GetInt() == 1
+			path := string(p.Get("id").GetStringBytes())
+			if len(path) > 1 && path[len(path)-1] == '.' {
+				path = path[0 : len(path)-1]
 			}
 			matches = append(matches, protov3.GlobMatch{
 				Path:   path,
