ecb1570d6cdd080632a0d1665551b4a5256e5710
Nicolás Pazos <npazosmendez@gmail.com>
2022-07-19 16:07:54 -0300

    Fix failing tests

--- expr/functions/divideSeries/function.go
+++ expr/functions/divideSeries/function.go
@@ -76,7 +76,7 @@ func (f *divideSeries) Do(ctx context.Context, e parser.Expr, from, until int64,
 
 	var results []*types.MetricData
 	for _, numerator := range numerators {
-		r := *numerator
+		r := numerator.CopyLink()
 		if useMetricNames {
 			r.Name = fmt.Sprintf("divideSeries(%s,%s)", numerator.Name, denominator.Name)
 		} else {
@@ -94,7 +94,7 @@ func (f *divideSeries) Do(ctx context.Context, e parser.Expr, from, until int64,
 
 			r.Values[i] = v / denominator.Values[i]
 		}
-		results = append(results, &r)
+		results = append(results, r)
 	}
 
 	return results, nil
