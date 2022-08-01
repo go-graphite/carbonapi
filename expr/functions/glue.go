2dde0b6b161d845818a103f62d1b61cec8ddceab
Carrie Edwards <edwrdscarrie@gmail.com>
2022-08-01 10:51:47 -0700

    Add support for Graphite web sigmoid() function (#15)
    * Add support for Graphite web sigmoid() function
    
    * Add tests for sigmoid() function
    
    * Update imports
    
    * Update sigmoid function to check for divide by zero
    
    * Update function to not modify input

--- expr/functions/glue.go
+++ expr/functions/glue.go
@@ -86,6 +86,7 @@ import (
 	"github.com/grafana/carbonapi/expr/functions/scaleToSeconds"
 	"github.com/grafana/carbonapi/expr/functions/seriesByTag"
 	"github.com/grafana/carbonapi/expr/functions/seriesList"
+	"github.com/grafana/carbonapi/expr/functions/sigmoid"
 	"github.com/grafana/carbonapi/expr/functions/sinFunction"
 	"github.com/grafana/carbonapi/expr/functions/slo"
 	"github.com/grafana/carbonapi/expr/functions/smartSummarize"
@@ -200,6 +201,7 @@ func New(configs map[string]string) {
 		{name: "scaleToSeconds", filename: "scaleToSeconds", order: scaleToSeconds.GetOrder(), f: scaleToSeconds.New},
 		{name: "seriesByTag", filename: "seriesByTag", order: seriesByTag.GetOrder(), f: seriesByTag.New},
 		{name: "seriesList", filename: "seriesList", order: seriesList.GetOrder(), f: seriesList.New},
+		{name: "sigmoid", filename: "sigmoid", order: sigmoid.GetOrder(), f: sigmoid.New},
 		{name: "sinFunction", filename: "sinFunction", order: sinFunction.GetOrder(), f: sinFunction.New},
 		{name: "slo", filename: "slo", order: slo.GetOrder(), f: slo.New},
 		{name: "smartSummarize", filename: "smartSummarize", order: smartSummarize.GetOrder(), f: smartSummarize.New},
