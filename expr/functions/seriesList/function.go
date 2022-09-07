cc70ffe33dad1ede17687871698aca74f8689889
Carrie Edwards <edwrdscarrie@gmail.com>
2022-09-07 11:52:18 -0700

    Align series by step in *seriesLists functions if steps or value length not equal (#99)
    Align series by step in divideBySeries if steps not equal

--- expr/functions/seriesList/function.go
+++ expr/functions/seriesList/function.go
@@ -2,7 +2,6 @@ package seriesList
 
 import (
 	"context"
-	"fmt"
 	"math"
 	"sort"
 	"strconv"
@@ -162,11 +161,16 @@ func (f *seriesList) Do(ctx context.Context, e parser.Expr, from, until int64, v
 			}
 		}
 		if pairFound {
-			if numerator.StepTime != denominator.StepTime || len(numerator.Values) != len(denominator.Values) {
-				return nil, fmt.Errorf("series %s must have the same length as %s", numerator.Name, denominator.Name)
+			pair := []*types.MetricData{numerator, denominator}
+			step, alignStep := helper.GetCommonStep(pair)
+			if alignStep || len(numerator.Values) != len(denominator.Values) {
+				alignedSeries := helper.ScaleToCommonStep(types.CopyMetricDataSlice(pair), step)
+				numerator = alignedSeries[0]
+				denominator = alignedSeries[1]
 			}
 		}
-		r := *numerator
+
+		r := numerator.CopyLink()
 		var denomName string
 		if pairFound {
 			denomName = denominator.Name
@@ -199,7 +203,7 @@ func (f *seriesList) Do(ctx context.Context, e parser.Expr, from, until int64, v
 				r.Values[i] = compute(v, denomValue)
 			}
 		}
-		results = append(results, &r)
+		results = append(results, r)
 	}
 	return results, nil
 }
