6b03de9de7899dab00da5ac1d92f4417a4adafc6
Nicolás Pazos <npazosmendez@gmail.com>
2022-09-09 15:58:42 -0300

    AggDiff consolidation: preallocate slice

--- expr/consolidations/consolidations.go
+++ expr/consolidations/consolidations.go
@@ -382,7 +382,7 @@ func AggCount(v []float64) float64 {
 }
 
 func AggDiff(v []float64) float64 {
-	safeValues := make([]float64, 0)
+	safeValues := make([]float64, 0, len(v))
 	for _, vv := range v {
 		if !math.IsNaN(vv) {
 			safeValues = append(safeValues, vv)
