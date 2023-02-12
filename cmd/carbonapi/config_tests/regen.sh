41b914fecc465c1cc4e228e6161b1d75a3a6f7ea
Vladimir Smirnov <civiloid@google.com>
2023-02-12 15:05:12 +0100

    [Test fix]: fix config tests
     * Client certificate now valid for 10 years
     * Scripts should work well on OSX now
     * Switch to RSA for signing algorithm as that is compatible with
       LibreSSL (default SSL library on current versions of MacOS)

--- cmd/carbonapi/config_tests/regen.sh
+++ cmd/carbonapi/config_tests/regen.sh
@@ -3,20 +3,24 @@
 echo "THIS IS INSECURE AND IS ONLY USED FOR Integration test purposes"
 echo "Never use it in production, please read how to do it properly!!!"
 
+set -e
+#ALGORITHM="Ed25519"
+ALGORITHM="rsa"
+
 # Create CA
-openssl genpkey -algorithm Ed25519 -out mTLS-server.key
+openssl genpkey -algorithm ${ALGORITHM} -out mTLS-server.key
 openssl req -new -x509 -sha256 -key mTLS-server.key -out mTLS-server.crt -days 3650 -subj '/CN=localhost' -addext "subjectAltName = DNS:localhost"
 
 echo "mTLS Test Server certificate has been created:" 
 openssl x509 -noout -text -in mTLS-server.crt
 
 # Client cert
-openssl genpkey -algorithm Ed25519 -out mTLS-client.key
+openssl genpkey -algorithm ${ALGORITHM} -out mTLS-client.key
 openssl req -new -key mTLS-client.key -out mTLS-client.csr -subj '/CN=test-uuid'
 
 # Sign our client certificate with our CA
 echo "00" > file.srl
-openssl x509 -req -in mTLS-client.csr -CA mTLS-server.crt -CAkey mTLS-server.key -CAserial file.srl -out mTLS-client.crt
+openssl x509 -days 3650 -req -in mTLS-client.csr -CA mTLS-server.crt -CAkey mTLS-server.key -CAserial file.srl -out mTLS-client.crt
 
 echo "mTLS Test Client certificate has been created:" 
 openssl x509 -noout -text -in mTLS-client.crt
