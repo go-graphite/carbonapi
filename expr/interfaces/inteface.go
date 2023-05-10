d0eecc2ae34be2bbcb895e41ada73cfc2ef6e78a
Justin Lei <97976793+leizor@users.noreply.github.com>
2023-05-10 12:18:43 -0700

    Amend docstring for Evaluator.Fetch
    Co-authored-by: Nicolás Pazos <32206519+npazosmendez@users.noreply.github.com>

--- expr/interfaces/inteface.go
+++ expr/interfaces/inteface.go
@@ -27,7 +27,7 @@ type Evaluator interface {
 	// Fetch populates the values map being passed into it by translating input expressions into a series of
 	// parser.MetricRequest and fetching the raw data from the configured backend.
 	//
-	// It returns a map of only the data fetched in the current invocation.
+	// It returns a map of only the data requested in the current invocation, scaled to a common step.
 	Fetch(ctx context.Context, e []parser.Expr, from, until int64, values map[parser.MetricRequest][]*types.MetricData) (map[parser.MetricRequest][]*types.MetricData, error)
 	// Eval uses the raw data within the values map being passed into it to in order to evaluate the input expression.
 	Eval(ctx context.Context, e parser.Expr, from, until int64, values map[parser.MetricRequest][]*types.MetricData) ([]*types.MetricData, error)
