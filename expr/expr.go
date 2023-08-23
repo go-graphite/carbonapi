d8f48ebba9ca4f8b0388e256da4ee86c6eb5b4f6
Evans Owamoyo <lordvidex.prime@gmail.com>
2023-08-23 12:41:59 +0300

    added consolidationFunc to metricRequest

--- expr/expr.go
+++ expr/expr.go
@@ -38,9 +38,16 @@ func (eval evaluator) Fetch(ctx context.Context, exprs []parser.Expr, from, unti
 				MaxDataPoints:  maxDataPoints,
 			}
 			metricRequest := parser.MetricRequest{
-				Metric: fetchRequest.PathExpression,
-				From:   fetchRequest.StartTime,
-				Until:  fetchRequest.StopTime,
+				Metric:            fetchRequest.PathExpression,
+				From:              fetchRequest.StartTime,
+				Until:             fetchRequest.StopTime,
+				ConsolidationFunc: m.ConsolidationFunc,
+			}
+
+			if m.ConsolidationFunc != "" {
+				fetchRequest.FilterFunctions = append(fetchRequest.FilterFunctions, &pb.FilteringFunction{
+					Name: m.ConsolidationFunc,
+				})
 			}
 
 			if exp.Target() == "fallbackSeries" {
